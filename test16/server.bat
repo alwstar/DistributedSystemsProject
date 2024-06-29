fa9f006ee9fe066358e29e7910c175c34b664c54
alwstar <60684855+alwstar@users.noreply.github.com>
2024-06-29 22:04:32 +0200

    test16

--- test16/server.bat
+++ test16/server.bat
@@ -1,2 +1,9 @@
-REM Open command line
-cmd /k "cd /d C:\git\DistributedSystemsProject\test16 && python server.py"
+@echo off
+REM Open server on port 6001
+start cmd /k "cd /d C:\git\DistributedSystemsProject\test16 && python server.py 6001"
+
+REM Open server on port 6002
+start cmd /k "cd /d C:\git\DistributedSystemsProject\test16 && python server.py 6002"
+
+REM Open server on port 6003
+start cmd /k "cd /d C:\git\DistributedSystemsProject\test16 && python server.py 6003"
---
+++ test20/server2.bat
@@ -0,0 +1,2 @@
+REM Open command line
+cmd /k "cd /d C:\git\DistributedSystemsProject\test20 && python server.py 6002"
